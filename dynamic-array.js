c6538676ece8d8455132467f3ef52feb8ef21e37
Kendra <cruickshank.kendra@gmail.com>
2024-05-08 16:24:24 -0700

    time complexity added

--- dynamic-array.js
+++ dynamic-array.js
@@ -9,10 +9,12 @@ class DynamicArray {
   }
 
   read(index) {
+    //O(1) time complexity 
     return this.data[index];
   }
 
   unshift(val) {
+    //O(n) time complexity 
     //need to shift over every element in the array to the right
     for (let i = this.data.length; i > 0; i--) {
       this.data[i] = this.data[i - 1]
